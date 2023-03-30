d41c4d97b42a4184f63001a813b254f3a7e68756
ryotaago <126906924+ryotaago@users.noreply.github.com>
2023-03-30 14:55:02 +0000

    Finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,28 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :is_matching_login_user, only: [:update]
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
   end
-
+  
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+       flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
     else
-      render "show"
+      render :edit
     end
   end
 
@@ -30,8 +34,6 @@ class UsersController < ApplicationController
 
   def ensure_correct_user
     @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to user_path(current_user)
-    end
+      redirect_to user_path(current_user) unless @user == current_user
   end
 end
